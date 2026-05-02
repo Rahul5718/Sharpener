f7b1c658430e55a56fac507756081fa2162e6b1a
Rahul <rahulkatravath03@gmail.com>
2026-05-02 15:24:57 +0530

    ecommers

---
+++ Backend/E-commers/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const userRoutes = require('./routes/user')
+const productRoutes = require('./routes/product')
+const cartRoutes = require('./routes/cart')
+
+app.use('/users', userRoutes)
+app.use('/products', productRoutes)
+app.use('/cart', cartRoutes)
+
+let port = 3000
+
+app.listen(port, () => {
+     console.log(`Server is running on port ${port}`)
+})
